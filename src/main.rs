e45a8a5ea72edac6dee0eed7802e19029d18b9fd
Suraj Ramchandran <30753067+Suraj-Ram@users.noreply.github.com>
2026-06-05 12:46:30 +0400

    child runs program passed in

--- src/main.rs
+++ src/main.rs
@@ -7,38 +7,41 @@ use nix::{
     unistd::{ForkResult, execvp, fork, write},
 };
 
-use std::ffi::{CStr, CString};
+use std::{
+    env,
+    ffi::{CStr, CString},
+};
+
+fn extract_prog_args() -> Vec<String> {
+    env::args().skip(1).collect()
+}
 
 fn main() {
     println!("Hello, world!");
-    let c_name = friendly_id::generate();
-    println!("Starting container {c_name}");
+    // let c_name = friendly_id::generate();
+    // println!("Starting container {c_name}");
 
-    // match unsafe { fork() } {
-    //     Ok(ForkResult::Parent { child, .. }) => {
-    //         println!(
-    //             "Continuing execution in parent process, new child has pid: {}",
-    //             child
-    //         );
-    //         waitpid(child, None).unwrap();
-    //     }
-    //     Ok(ForkResult::Child) => {
-    //         // Unsafe to use `println!` (or `unwrap`) here. See Safety.
-    //         write(std::io::stdout(), "I'm a new child process\n".as_bytes()).ok();
-    //         unsafe { libc::_exit(1) };
-    //     }
-    //     Err(_) => println!("Fork failed"),
-    // }
-    //
-    //
-    // let program: &str = "/bin/ls";
-    // let prog_c_string = CString::new(program).unwrap();
-    // let prog_c_str = prog_c_string.as_c_str();
+    let prog_args: Vec<CString> = extract_prog_args()
+        .into_iter()
+        .map(|x| CString::new(x).expect("Received bad string"))
+        .collect();
 
-    // let _ = execvp(&c"/bin/ls", &[c"-a"]);
-    let _ = execvp(&c"/bin/pwd", &[c"-a"]);
+    dbg!(&prog_args);
 
-    println!("this should not be printed");
-
-    // let _ = execvp(&c"/bin/ls", &[c"-a"]);
+    match unsafe { fork() } {
+        Ok(ForkResult::Parent { child, .. }) => {
+            println!(
+                "Continuing execution in parent process, new child has pid: {}",
+                child
+            );
+            waitpid(child, None).unwrap();
+        }
+        Ok(ForkResult::Child) => {
+            // Unsafe to use `println!` (or `unwrap`) here. See Safety.
+            // write(std::io::stdout(), "I'm a new child process\n".as_bytes()).ok();
+            println!("in child");
+            let _ = execvp(&prog_args[0], &prog_args);
+        }
+        Err(_) => println!("Fork failed"),
+    }
 }
