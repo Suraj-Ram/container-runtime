5a13d06634c7c73dc3f91f1ff7973a3f41d5281b
Suraj Ramchandran <30753067+Suraj-Ram@users.noreply.github.com>
2026-06-17 13:58:12 +0400

    tests for friendly_id. container runtime refactor WIP

--- src/friendly_id.rs
+++ src/friendly_id.rs
@@ -1,4 +1,3 @@
-
 use rand::RngExt;
 
 const ADJECTIVES: &str = "amber, ancient, arctic, autumn, azure, blazing, bold, brave, bright, calm, cedar, celestial, charming, chill, clean, clever, cloud, coastal, cobalt, cool, copper, coral, cosmic, crisp, crystal, daring, dawn, deep, deft, dew, distant, divine, drift, dusk, eager, early, emerald, epic, eternal, faint, fierce, fleet, floating, forest, fresh, frost, gentle, gilded, glacial, glowing, golden, grand, green, hallowed, hidden, high, hollow, humble, icy, idle, ivory, jade, keen, kindred, lofty, lone, loyal, lunar, majestic, maple, marble, mellow, mighty, misty, noble, northern, oak, open, pale, patient, peaceful, pine, polar, proud, quiet, radiant, rapid, roaming, rocky, royal, rugged, rustic, sage, serene, silent, silver, sleek, solar, steady, still, stone, stormy, sunlit, swift, tidal, timid, twilight, vast, velvet, verdant, wandering, warm, wild, windy, winter, wise, woven, zeal";
@@ -23,4 +22,16 @@ pub fn generate() -> String {
         get_one(NOUNS),
         rand::rng().random_range(0..=999)
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_3_parts() {
+        let id = generate();
+        let parts: Vec<&str> = id.split("-").collect();
+        assert_eq!(parts.len(), 3);
+    }
 }
